eb223d0bbb0ad653c75d9769a70d6ed4be30d4bd
ken <kenken17@gmail.com>
2024-09-16 16:34:13 +0800

    Remove check upstream

--- src/worktree_command.sh
+++ src/worktree_command.sh
@@ -5,14 +5,6 @@ worktreeDest=~/Worktrees/$branch/$folderName
 
 baseBranch=$(git branch -l main master --format '%(refname:short)')
 
-# default push up stream
-noUpStream=0
-
-# check remote branch exist
-if git rev-parse --verify --quiet origin/"$branch" >/dev/null 2>&1; then
-  noUpStream=1
-fi
-
 echo "Create new worktree for $(green "$branch")"
 
 if [[ $worktreePath ]]; then
@@ -29,6 +21,4 @@ git worktree add -b "$branch" "$worktreeDest" origin/"$baseBranch"
 mkdir -p "$worktreeDest" && cd "$_" || exit
 
 # set upstream
-if [[ $noUpStream == 0 ]]; then
-  git push --set-upstream origin "$branch"
-fi
+git push --set-upstream origin "$branch"
--- t
+++ t
@@ -222,14 +222,6 @@ t_worktree_command() {
 
   baseBranch=$(git branch -l main master --format '%(refname:short)')
 
-  # default push up stream
-  noUpStream=0
-
-  # check remote branch exist
-  if git rev-parse --verify --quiet origin/"$branch" >/dev/null 2>&1; then
-    noUpStream=1
-  fi
-
   echo "Create new worktree for $(green "$branch")"
 
   if [[ $worktreePath ]]; then
@@ -246,9 +238,7 @@ t_worktree_command() {
   mkdir -p "$worktreeDest" && cd "$_" || exit
 
   # set upstream
-  if [[ $noUpStream == 0 ]]; then
-    git push --set-upstream origin "$branch"
-  fi
+  git push --set-upstream origin "$branch"
 
 }
 
